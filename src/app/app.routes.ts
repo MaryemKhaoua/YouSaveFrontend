30434be33974b27b950a618837d05dee02db8380
Maryem khaoua <maryem.khaoua@gmail.com>
2025-03-06 14:52:27 +0000

    add route for dashboard and blood

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,16 @@ import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AuthGuard } from './guards/auth.guard';
+import { BloodListComponent } from './components/blood/blood-list/blood-list.component';
+import { BloodFormComponent } from './components/blood/blood-form/blood-form.component';
 
 export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  // { path: 'donors', component: DonorsComponent },
-  // { path: 'news-feed', component: NewsFeedComponent },
-  // { path: 'about', component: AboutComponent },
-  // { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: 'blood', component: BloodListComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
+  { path: 'blood/new', component: BloodFormComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
+  { path: 'blood/edit/:id', component: BloodFormComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' }
 ];
