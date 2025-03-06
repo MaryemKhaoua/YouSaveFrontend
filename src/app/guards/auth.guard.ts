c9d5ab15061691879a929fab3c4ab8054b1bb1ca
Maryem khaoua <maryem.khaoua@gmail.com>
2025-03-06 14:51:00 +0000

    fixed some error about roles

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -8,8 +8,16 @@ import { AuthService } from '../services/auth.service';
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
-    if (this.authService.getToken()) {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const token = this.authService.getToken();
+    const role = this.authService.getRoleFromToken();
+  
+    if (token && role) {
+      if (route.data['roles'] && route.data['roles'].indexOf(role) === -1) {
+        this.router.navigate(['/login']);
+        return false;
+      }
+  
       return true;
     } else {
       this.router.navigate(['/login']);
