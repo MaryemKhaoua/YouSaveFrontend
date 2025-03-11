c2030762a533e9d56cb051f7a0711251906656fd
Maryem khaoua <maryem.khaoua@gmail.com>
2025-03-11 20:48:48 +0000

    created home page

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../layout/navbar/navbar.component";
+import { FooterComponent } from "../../layout/footer/footer.component";
+
+@Component({
+  selector: 'app-home',
+  imports: [NavbarComponent, FooterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
