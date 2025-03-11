3d3be9e330950a8dd3397d75cb9b8145d28a4219
Maryem khaoua <maryem.khaoua@gmail.com>
2025-03-11 20:48:09 +0000

    route for home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,16 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AuthGuard } from './guards/auth.guard';
 import { BloodListComponent } from './components/blood/blood-list/blood-list.component';
 import { BloodFormComponent } from './components/blood/blood-form/blood-form.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'blood', component: BloodListComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
   { path: 'blood/new', component: BloodFormComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
   { path: 'blood/edit/:id', component: BloodFormComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' }
 ];
